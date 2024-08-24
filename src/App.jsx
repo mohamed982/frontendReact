297e50901427bf74734119737bef09943fd96017
mohamed nabil <mohd.nabil982@hotmail.com>
2024-08-24 17:27:38 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,19 @@
 import { useEffect, useState } from 'react'
-
-const Component1=()=>{
-  return <h1 style={{color:'orangered'}}>Componet 1</h1>
-}
-const Component2=()=>{
-  return <h1 style={{color:'blue'}}>Componet 2</h1>
-}
-const Component3=()=>{
-  return <h1 style={{color:'yello'}}>Componet 3</h1>
-}
-const Component4=()=>{
-  return <h1 style={{color:'green'}}>Componet 4</h1>
-}
-
+import "./main.css"
 
 function App() {
-  const [data,setData]=useState([]);
-
-  const getData=()=>{
-    return new Promise((res,rej)=>{
-      setTimeout(()=>{
-        return res(['Component1', 'Component2', 'Component3', 'Component4']);
-      },2000)
-    })
-  }
-
-  const handleChange=(e)=>{
-
-  }
-  
-  useEffect(()=>{
-    (async()=>{
-      const data=await getData();
-      setData(data);
-    })();
-  },[]);
-
   return (
     <>
-      <div>
-          {/* Render Components Here */}
-
-          {/* Upload Photo */}
-          <div>
-             <img
-                src=''
-             />
-             <input type="file" onChange={handleChange}/>
-          </div>
+      <div className='layout'>
+        <div className="slot">
+          <div className="widget">Task 1</div>
+        </div>
+        <div className="slot">
+          <div className="widget">Task 2</div>
+        </div>
+        <div className="slot">
+          <div className="widget">Task 3</div>
+        </div>
       </div>
     </>
   )
